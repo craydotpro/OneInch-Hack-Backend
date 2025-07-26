9094489714a3467f34ed365cc261a597f0558447
ptsayli <ptsayli@gmail.com>
2025-07-26 17:08:16 +0400

    return order to sign

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -58,6 +58,7 @@ router.post('/prepare-buy', async (req: Request, res: Response) => {
     const position = await Position.create(positionParams)
     // return order data to sign
     res.json({
+      data: order,
       'message': 'Position prepared successfully',
     })
   } catch (err) {
@@ -66,86 +67,19 @@ router.post('/prepare-buy', async (req: Request, res: Response) => {
   }
 })
 
-// router.post('/sell', async (req: Request, res: Response) => {
-//   try {
-//     // buy / (sell- relayer)
-//     const {
-//       userAddress,
-//       toToken,
-//       type,
-//       amountInUSD,
-//       triggerPrice,
-//       advanceSLTP
-//     } = req.body
-
-//     // get user balance
-//     // get gasprice
-//     // get token price
-
-//     const route = await prepareSmartBuyRoute(userAddress, toToken, amountInUSD) //return destination details
-//     // create and store orderHash
-//     const positionParams = {
-//       userAddress,
-//       toToken,
-//       type,
-//       amountInUSD,
-//       qty: route.estimatedQty,
-//       slippage: 0.5,
-//       triggerPrice,
-//       advanceSLTP,
-//       chain: route.fromChain,
-//       deadline: Math.floor(Date.now() / 1000) + 300
-//     }
-
-//     // save positionParams to database
-//     // prepare order to sign
-//     const payParams = {
-//       senderAddress: userAddress,
-//       receiverAddress: userAddress,
-//       amount: amountInUSD, // amount in USD
-//       destinationChain, // comes from route
-//       destinationToken, // comes from route
-//       orderType: 'p2p', // dapp
-//     }
-//     // Create Order
-//     const order = await createOrder(payParams)
-//     // return order data to sign
-//     res.json({
-//       'message': 'Position prepared successfully',
-//     })
-//   } catch (err) {
-//     console.error('[preparePosition]', err)
-//     res.status(500).json({ error: 'Failed to prepare position' })
-//   }
-// })
-
 // POST /api/position/submit
-// router.post('/submit', async (req: Request, res: Response) => {
-//   try {
-//     const { signedPayload, signature, swapData, routeInfo } = req.body
+router.post('/submit', async (req: Request, res: Response) => {
+  try {
+    const { signedPayload, signature} = req.body
 
-//     const valid = verifySignature(signedPayload.wallet, signedPayload, signature)
-//     if (!valid) {
-//       return res.status(400).json({ error: 'Invalid signature' })
-//     }
 
-//     const position = await Position.create({
-//       ...signedPayload,
-//       status: 'pending',
-//       signature,
-//       routeInfo,
-//       createdAt: new Date()
-//     })
-
-//     res.json({
-//       positionId: position._id,
-//       status: position.status
-//     })
-//   } catch (err) {
-//     console.error('[submitPosition]', err)
-//     res.status(500).json({ error: 'Failed to submit position' })
-//   }
-// })
+    res.json({      message: 'Position submitted successfully',
+    })
+  } catch (err) {
+    console.error('[submitPosition]', err)
+    res.status(500).json({ error: 'Failed to submit position' })
+  }
+})
 
 
 export default router
