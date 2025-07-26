b0984d070b42919ee07977ea9d697078986c752c
ptsayli <ptsayli@gmail.com>
2025-07-26 15:52:23 +0400

    fix: crash

--- src/utils/getWallets.ts
+++ src/utils/getWallets.ts
@@ -1,6 +1,7 @@
 // @todo: remove ethers
 import { Account, Hex } from "viem"
 import { privateKeyToAccount } from "viem/accounts"
+import { chains } from "../config/chainConfig"
 import { providers } from "../config/rpcProvider"
 [
     process.env.TESTNET_SOLVER_PRIVATE_KEY,
@@ -14,23 +15,15 @@ import { providers } from "../config/rpcProvider"
         throw Error("Missing private keys")
     }
 })
-export let CHAINS = {} as any
 
 const WALLETS = {
     solver:{},
     owner:{},
     relayer:{}
 } as any
-fetch(`${process.env.GATEWAY_URL}/api/chains`, {
-    method: 'GET',
-    headers: {
-    'Content-Type': 'text/event-stream',
-    },
-}).then(async data=>{
-    data = await data.json()
-    CHAINS = (data as any).result.chains
+
     Object.keys(providers).reduce((obj,chainId)=>{
-        const testnet = CHAINS[chainId].testnet
+        const testnet = chains[chainId]?.testnet || false
         const solverPrivateKey = testnet ? process.env.TESTNET_SOLVER_PRIVATE_KEY!: process.env.MAINNET_SOLVER_PRIVATE_KEY!
         const ownerPrivateKey = testnet ? process.env.TESTNET_OWNER_PRIVATE_KEY! : process.env.MAINNET_OWNER_PRIVATE_KEY!
         const relayerPrivateKey = testnet ? process.env.TESTNET_RELAYER_PRIVATE_KEY! : process.env.MAINNET_RELAYER_PRIVATE_KEY!
@@ -40,7 +33,7 @@ fetch(`${process.env.GATEWAY_URL}/api/chains`, {
 
         return obj
     }, WALLETS)
-})
+
 
 export const getRelayerAccountByChainId = (chainId: number): Account => {
     if (!(chainId in WALLETS.relayer)) throw new Error(`invalid ChainId`)
