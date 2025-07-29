083056d63caf42d4b695db3dc3ea523b2b11fa8d
ptsayli <ptsayli@gmail.com>
2025-07-29 12:59:04 +0400

    add: set sl/tp, return data to sign, submit signed sl/tp data

---
+++ src/apis/helpers/sltp.ts
@@ -0,0 +1,49 @@
+
+export const signSLTPPosition = (account: any, position: any) => {
+  const domain = {
+    name: "AdvancePosition",
+    version: "1",
+  }
+  const types = {
+    AdvancePosition: [
+      { name: "maker", type: "address" },
+      { name: "makerAsset", type: "address" },
+      { name: "makerAmount", type: "uint256" },
+      { name: "takerAsset", type: "address" },
+      { name: "triggerPrice", type: "uint256" },
+      { name: "deadline", type: "uint256" },
+      { name: "isStopLoss", type: "bool" },
+    ],
+  };
+  console.log('msg', position)
+  return account.signTypedData({
+    domain,
+    types,
+    primaryType: "AdvancePosition",
+    message: position,
+  });
+}
+
+export const prepareSLTPPosition = ( position: any) => {
+  const domain = {
+    name: "AdvancePosition",
+    version: "1",
+  }
+  const types = {
+    AdvancePosition: [
+      { name: "maker", type: "address" },
+      { name: "makerAsset", type: "address" },
+      { name: "makerAmount", type: "uint256" },
+      { name: "takerAsset", type: "address" },
+      { name: "triggerPrice", type: "uint256" },
+      { name: "deadline", type: "uint256" },
+      { name: "isStopLoss", type: "bool" },
+    ],
+  };
+  return{
+    domain,
+    types,
+    primaryType: "AdvancePosition",
+    message: position,
+  }
+}
