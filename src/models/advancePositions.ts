bffd3ae79b549b62a43ebaa78cafada5b777b4c7
ptsayli <ptsayli@gmail.com>
2025-07-29 12:58:01 +0400

    add: sl/tp collection

---
+++ src/models/advancePositions.ts
@@ -0,0 +1,47 @@
+import { model, Schema } from "mongoose";
+
+export enum AdvanceSLTPType {
+  SL = 'sl',
+  TP = 'tp'
+}
+const AdvanceSLTPSchema = new Schema({
+  positionId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Position',
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: AdvanceSLTPType,
+    required: true,
+  },
+  positionData: {
+    type: String,
+    required: true,
+  },
+  signedPosition: {
+    type: String,
+    required: false,
+  },
+  status: {
+    type: String,
+    enum: ['active', 'executed', 'cancelled', 'failed'],
+    default: 'active'
+  },
+  executedAt: {
+    type: Date,
+    required: false
+  },
+  txHash: {
+    type: String,
+    required: false
+  },
+}, {
+  timestamps: true,
+  strict: false,
+  toObject:{ getters: true },
+  toJSON: { getters: true }
+})
+
+export const AdvanceSLTP = model('AdvanceSLTP', AdvanceSLTPSchema)
+
